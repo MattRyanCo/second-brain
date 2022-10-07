43e3ba16a7838e5389a0b517765b12c2c299092e
Matt Ryan <matt.ryan@webdevstudios.com>
2022-10-07 16:50:52 -0400

    Reset for Ironman

--- obsidian-make-daily.py
+++ obsidian-make-daily.py
@@ -113,65 +113,75 @@ def get_open_todos():
 						open[m] = fi_open[m]
 	return open
 
+def get_current_year():
+	thisyear = ""
+	return thisyear
+
 # Put it all together
 # home_root = os.path.expanduser('~')
-home_root = "/mnt/e/OB-SecondBrain"
+home_root = "/mnt/c/Users/mattr/Documents/Obsidian/OB-SecondBrain-WDS"
 config_file = "_config/.notes.md"
-open_tasks_template_file = "/mnt/e/OB-SecondBrain/_templates/open-tasks.md"
+open_tasks_template_file = home_root + "/_templates/OpenTasks.md"
 
+# daily_notes_root s/b relative to the root of the vault
+#  ie  /__BulletJournal/2022
 daily_notes = get_config_value("daily_notes_root")
+daily_notes_path = home_root + daily_notes
 weather_location = get_config_value("weather_zip")
-daily_notes_file = os.path.join(daily_notes, get_daily_notes_filename())
+daily_notes_file = os.path.join(daily_notes_path, get_daily_notes_filename())
 
-if os.path.exists(daily_notes_file):
-	print("File already exists. Not overwriting...")
-else:
-	print("Generating daily notes file " + os.path.basename(daily_notes_file) + "...")
-	with open(daily_notes_file, 'w') as fh:
-		tasks = {}
-		# agenda = get_agenda().split("\n")
-# Set UP YAML frontmatter at very top of file
-		fh.write("---\ntags: DailyNote\n---\n")
-# Build out Yesterday|Tomorrow navigation with weather
-		agenda = get_calendar_events.main().split("\n")
-# Make note navigation
-		nav_bar = get_link_for_file(get_daily_notes_filename(offset=-1))
-		nav_bar += " | " + get_link_for_file(get_daily_notes_filename(offset=1))
-		nav_bar += " | " + get_weather(weather_location)
-		fh.write(nav_bar + "\n")
-# Grab the scheduled items from Google Calendar
-		fh.write("\n## Agenda\n")
-		if len(agenda) == 1:
-			fh.write("Nothing in today's calendar\n")
-		else:
-			for item in agenda:
-				fh.write(item + "\n")
+try: 
+	if os.path.exists(daily_notes_file):
+		print("File already exists. Not overwriting...")
+	else:
+		print("Generating daily notes file " + os.path.basename(daily_notes_file) + "...")
+		with open(daily_notes_file, 'w') as fh:
+			tasks = {}
+			# agenda = get_agenda().split("\n")
+	# Set UP YAML frontmatter at very top of file
+			fh.write("---\ntags: DailyNote\n---\n")
+	# Build out Yesterday|Tomorrow navigation with weather
+			agenda = get_calendar_events.main().split("\n")
+	# Make note navigation
+			nav_bar = get_link_for_file(get_daily_notes_filename(offset=-1))
+			nav_bar += " | " + get_link_for_file(get_daily_notes_filename(offset=1))
+			nav_bar += " | " + get_weather(weather_location)
+			fh.write(nav_bar + "\n")
+	# Grab the scheduled items from Google Calendar
+			fh.write("\n## Agenda\n")
+			if len(agenda) == 1:
+				fh.write("Nothing in today's calendar\n")
+			else:
+				for item in agenda:
+					fh.write(item + "\n")
 
-# Output basic headings for today's notes.
-#
-		fh.write("\n## Readings & Keeps\n")
-		fh.write(". \n")
-#
-		fh.write("\n## Today's notes & musings\n")
-		fh.write("- \n")
-#
-		fh.write("\n---\n")
-		fh.write("\n## To-Do\n")
-#
-#   Use daily notes & dataview format for listing open tasks. 
-		fh.write("\n\n---\n")
-		fh.write("### Past Due\n ```tasks\nnot done\ndue before today\n```\n")
-		fh.write("\n\n---\n")
-		fh.write("### Due Today\n ```tasks\nnot done\ndue today\n```\n")
-		fh.write("\n\n---\n")
-		fh.write("### Due Within 2 Weeks\n ```tasks\nnot done\ndue after today\ndue before in 2 weeks\n```\n")
-		fh.write("\n\n---\n")
-		fh.write("### Due Later\n ```tasks\nnot done\ndue after in 2 weeks\n```\n")
-		fh.write("\n\n---\n")
-		fh.write("### No Due Dates\n ```tasks\nnot done\nno due date\nheading does not include routine\n```")
-		fh.write("\n\n---\n")
-		fh.write("### Recently Created Notes\n ```dataview\nList\nFROM \"\" AND -#DailyNote\nWHERE file.ctime >= date(today) - dur(3 day)\nLIMIT 10\nSORT file.ctime\n```\n")
-		fh.write("\n\n---\n")
+	# Output basic headings for today's notes.
+	#
+			fh.write("\n## Readings & Keeps\n")
+			fh.write(". \n")
+	#
+			fh.write("\n## Today's notes & musings\n")
+			fh.write("- \n")
+	#
+			fh.write("\n---\n")
+			fh.write("\n## To-Do\n")
+	#
+	#   Use daily notes & dataview format for listing open tasks. 
+			fh.write("\n\n---\n")
+			fh.write("### Past Due\n ```tasks\nnot done\ndue before today\n```\n")
+			fh.write("\n\n---\n")
+			fh.write("### Due Today\n ```tasks\nnot done\ndue today\n```\n")
+			fh.write("\n\n---\n")
+			fh.write("### Due Within 2 Weeks\n ```tasks\nnot done\ndue after today\ndue before in 2 weeks\n```\n")
+			fh.write("\n\n---\n")
+			fh.write("### Due Later\n ```tasks\nnot done\ndue after in 2 weeks\n```\n")
+			fh.write("\n\n---\n")
+			fh.write("### No Due Dates\n ```tasks\nnot done\nno due date\nheading does not include routine\n```")
+			fh.write("\n\n---\n")
+			fh.write("### Recently Created Notes\n ```dataview\nList\nFROM \"\" AND -#DailyNote\nWHERE file.ctime >= date(today) - dur(3 day)\nLIMIT 10\nSORT file.ctime\n```\n")
+			fh.write("\n\n---\n")
+except:
+	print("Not Found")
 #
 # Future @todo: Read in the commands from the open-tasks template file and echo here
 #   rather than recreating the commands here. This to remove the need to edit 
